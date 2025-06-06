29fbcaefadb0dfbacdfec424dd5c20c4a2279d64
unbenounced <em845@nau.edu>
2025-06-05 22:41:02 -0400

    fixed pitcher spelling error

--- statcast_dataset.py
+++ statcast_dataset.py
@@ -5,7 +5,8 @@ import get_player as gp
 
 
 def loadSav(startdate,enddate):
+    # format = YYYY-MM-DD
     sav = pyb.statcast(start_dt=startdate,end_dt=enddate)
     sav['BatterName'] = sav['batter'].map(gp.id_dic())
-    sav['player_name'] = sav['pitcer'].map(gp.id_dic())
+    sav['player_name'] = sav['pitcher'].map(gp.id_dic())
     return(sav)
